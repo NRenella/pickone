662936230020b49dbb032b8636c14d10eb6e97b3
Noah Renella <ntrenella@cpp.edu>
2023-11-30 16:56:50 -0800

    Login Page Progress

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pickone/components/my_text_field.dart';
+import 'package:pickone/components/my_button.dart';
 
 class LoginPage extends StatefulWidget{
   const LoginPage({super.key});
@@ -8,26 +10,56 @@ class LoginPage extends StatefulWidget{
 }
 
 class _LoginPageState extends State<LoginPage>{
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              Icon(
-                Icons.message,
-                size: 80,
-              ),
-              // welcome back message
-              // email text field
-              // password text field
-              // sign in button
-              // not a member? register now
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 50,),
+                Icon(
+                  Icons.message,
+                  size: 80,
+                  color: Colors.grey[800],
+                ),
+                const SizedBox(height: 50,),
+                // welcome back message
+                Text(
+                  "Welcome Back I Missed You!",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 25,),
+                // email text field
+                MyTextField(
+                    controller: emailController,
+                    hintText: 'Email',
+                    obscureText: false),
+                const SizedBox(height: 10,),
+                // password text field
+                MyTextField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: true),
+                const SizedBox(height: 25,),
+                MyButton(onTap: (){}, text: "Sign In")
+
+                // sign in button
+                // not a member? register now
+              ],
+            ),
           ),
-        );
-      );
+        ),
+      ),
     );
   }
 }
