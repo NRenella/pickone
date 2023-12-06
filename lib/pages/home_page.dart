14fd97b04ddc87f5df40846725f89000f2be25df
Noah Renella <ntrenella@cpp.edu>
2023-12-05 17:54:22 -0800

    Message Features Added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:pickone/services/auth/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pickone/pages/chat_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class HomePage extends StatefulWidget{
   const HomePage({super.key});
@@ -10,6 +14,8 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage> {
+  // instance of auth
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   void signOut(){
     final authService = Provider.of<AuthService>(context, listen: false);
@@ -30,6 +36,53 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
+      body: _buildUserList(),
     );
+  }
+
+  Widget _buildUserList(){
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('user').snapshots(),
+      builder: (context, snapshot){
+        if(snapshot.hasError){
+          return const Text('error');
+        }
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return const Text('loading...');
+        }
+
+        return ListView(
+          children: snapshot.data!.docs
+            .map<Widget>((doc) => _buildUserListItem(doc))
+            .toList(),
+        );
+      },
+    );
+  }
+  // build individual user list item
+  Widget _buildUserListItem(DocumentSnapshot document){
+    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+    // display all users except current
+    if(_auth.currentUser!.email != data['email']){
+      return ListTile(
+        title: Text(data['email']),
+        onTap: (){
+          // pass the clicked user UID to the chatpage
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ChatPage(
+                receiveUserEmail:data['email'],
+                receiveUserID:data['uid'],
+              ),
+            ),
+          );
+        },
+      );
+    }
+    else {
+      return Container();
+    }
   }
 }
